3054a1c38d8d98896e98ba8a4ef6c1a7336b33ae
Arthur <jazzman.a@ya.ru>
2023-01-16 12:29:37 +0300

    16.11

--- bitrix/managed_cache/MYSQL/agents/76/76ee4ad3b12ba3471b5273d26f48b51b.php
+++ bitrix/managed_cache/MYSQL/agents/76/76ee4ad3b12ba3471b5273d26f48b51b.php
@@ -1,7 +1,7 @@
 <?
 if($INCLUDE_FROM_CACHE!='Y')return false;
-$datecreate = '001673856517';
-$dateexpire = '001673860177';
-$ser_content = 'a:2:{s:7:"CONTENT";s:0:"";s:4:"VARS";i:1673856537;}';
+$datecreate = '001673856553';
+$dateexpire = '001673860213';
+$ser_content = 'a:2:{s:7:"CONTENT";s:0:"";s:4:"VARS";i:1673856612;}';
 return true;
 ?>
